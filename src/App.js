a0f577b840c13d2f09255282407d7e31c3887395
Björn Gaworski <bg@tty7.de>
2020-08-19 22:08:51 +0200

    Cosmetic fixes

--- src/App.js
+++ src/App.js
@@ -1,17 +1,28 @@
 import React from 'react'
-import styled from 'styled-components'
+import styled, { createGlobalStyle } from 'styled-components'
 import Button from './button'
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    background-color: #e7e7e7;
+    margin: 0;
+    padding: 0;
+  }
+`
 
 const Div = styled.div`
   @import url('https://fonts.googleapis.com/css2?family=Roboto&display=swap');
 
-  background-color:#e7e7e7;
   display: flex;
-  justify-content: center;
+  justify-content: space-between;
   align-items: center;
   flex-direction: column;
   height: 100vh;
+  width:  100vw;
   font-family: 'Roboto', sans-serif;
+  overflow: hidden;
+  padding: 48px;
+  box-sizing: border-box;
   
   h1 {
     display: flex;
@@ -26,7 +37,7 @@ const Div = styled.div`
     }
   }
   
-  > div {
+  .card {
     display: flex;
     justify-content: center;
     align-items: center;
@@ -35,17 +46,15 @@ const Div = styled.div`
     background-color: #ffffff;
     border-radius: 4px;
     width: 50vw;
-    height: 25vh;
+    height: 20vh;
     color: #3f3f3f;
   }
   
-  > div:nth-child(3) {
-    margin-top: 18px;
+  .card:nth-child(3) {
     background-color: #bcbcbc;
   }
   
-  > div:last-child {
-    margin-top: 18px;
+  .card:last-child {
     background-color: #3f3f3f;
     color: #ffffff;
   }
@@ -65,12 +74,17 @@ function App() {
   );
 
   return (
-    <Div>
-      <h1>Buttons <input type="checkbox" value={useHover} onChange={e => setUseHover(e.target.checked)}/><span>Use Hover</span></h1>
-      <div><ButtonSet /></div>
-      <div><ButtonSet /></div>
-      <div><ButtonSet/></div>
-    </Div>
+    <>
+      <GlobalStyle />
+      <Div>
+        <div>
+          <h1>Buttons <input type="checkbox" value={useHover} onChange={e => setUseHover(e.target.checked)}/><span>Use Hover</span></h1>
+        </div>
+        <div className="card"><ButtonSet /></div>
+        <div className="card"><ButtonSet /></div>
+        <div className="card"><ButtonSet/></div>
+      </Div>
+    </>
   );
 }
 
