92b060a8f1f6a436508db20fea3aea429140c757
Björn Gaworski <bg@tty7.de>
2020-09-18 09:21:12 +0200

    Updates

---
+++ precache-manifest.bed9f34e29893c1a863069f902f928ea.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "faf7b92d811432cf8ebc87cc3ef3a103",
+    "url": "/apps-mocks/index.html"
+  },
+  {
+    "revision": "bcd9ecf6b172b2a12735",
+    "url": "/apps-mocks/static/js/2.3e2b84d2.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/apps-mocks/static/js/2.3e2b84d2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3cb5d56f108e2a706888",
+    "url": "/apps-mocks/static/js/main.a094437b.chunk.js"
+  },
+  {
+    "revision": "18b087c5e91182f08176",
+    "url": "/apps-mocks/static/js/runtime-main.380d59d9.js"
+  },
+  {
+    "revision": "7082a8029667c4c10a56b6230544193c",
+    "url": "/apps-mocks/static/media/burger-icon.7082a802.svg"
+  },
+  {
+    "revision": "c0dea19cf9fd8b028e2f333201de96ee",
+    "url": "/apps-mocks/static/media/flauschhoernchen.c0dea19c.png"
+  },
+  {
+    "revision": "bf637da490f87bea20c76075e9deafb8",
+    "url": "/apps-mocks/static/media/search_16.bf637da4.svg"
+  }
+]);
